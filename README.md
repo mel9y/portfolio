08e2f8cc737397ce510a366173d8f8a4bb03fe76
m2en <me@m2en.dev>
2023-03-02 22:36:01 +0900

    fix: change image unoptimized setting

--- README.md
+++ README.md
@@ -8,5 +8,3 @@ It can be started on the development server by following the steps below
 yarn
 yarn dev
 ```
-
-Use [Next.js](https://nextjs.org/) and utilize [Nextra](https://nextra.site/)'s blog theme for our portfolio.
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/src'
+  basePath: '/src',
+  images: {
+    unoptimized: true
+  }
 };
 
 const withNextra = require('nextra')({
